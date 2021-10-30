aa2876188016ae2c24e0d54a2c899701657442fd
unknown <jenearly@gmail.com>
2021-10-29 23:14:45 -0400

    remove images and add clear functionality

--- backend/helpers/index.js
+++ backend/helpers/index.js
@@ -4,54 +4,72 @@ database.loadDatabase();
 
 //This is for retrieving all data stored in database.db
 const neDBAll = () => {
-    return new Promise((resolve, reject) => {
-        database.find({}, (err, docs) => {
-            if (err) reject(err);
-            resolve(docs);
-        })
-    }).catch((err) =>  new Error('getting all went wrong' + err))
-}
-
+  return new Promise((resolve, reject) => {
+    database.find({}, (err, docs) => {
+      if (err) reject(err);
+      resolve(docs);
+    });
+  }).catch((err) => new Error("getting all went wrong" + err));
+};
 
 //This is for getting the currect number of indexes stored in database.db
 const neDBCount = () => {
-    return new Promise((resolve, reject) => {
-        database.find({}, (err, docs) => {
-            if (err) reject(err);
-            resolve(docs.length);
-        })
-    }).catch((err) =>  new Error('count went wrong' + err))
-}
+  return new Promise((resolve, reject) => {
+    database.find({}, (err, docs) => {
+      if (err) reject(err);
+      resolve(docs.length);
+    });
+  }).catch((err) => new Error("count went wrong" + err));
+};
 
 //This is for adding data into database.db
-const neDBAdd = async ( data) => {
-    const count = await neDBCount(database)
-    
-    return new Promise((resolve, reject) => {
-        database.insert({...data, _id: count + 1}, (err, newDoc) => {
-            if (err) reject(err);
-            resolve(newDoc)
-        })
-    }).catch((err) =>  new Error('add went wrong' + err))
-}
+const neDBAdd = async (data) => {
+  const count = await neDBCount(database);
+
+  return new Promise((resolve, reject) => {
+    database.insert({ ...data, _id: count + 1 }, (err, newDoc) => {
+      if (err) reject(err);
+      resolve(newDoc);
+    });
+  }).catch((err) => new Error("add went wrong" + err));
+};
 
 //This is for deleting a data in database.db
-const neDBRemove = async ({_id}) => {
-    return new Promise((resolve, reject) => {
-        const idINT = parseInt(_id)
+const neDBRemove = async ({ _id }) => {
+  return new Promise((resolve, reject) => {
+    const idINT = parseInt(_id);
 
-        database.remove({_id: idINT}, (err, numRemoved) => {
-            if (err) reject(err);
-            resolve(numRemoved)
-        })
-        database.persistence.compactDatafile()
-    }).catch((err) => new Error('remove went wrong' + err) )
-}
+    database.remove({ _id: idINT }, (err, numRemoved) => {
+      if (err) reject(err);
+      resolve(numRemoved);
+    });
+    database.persistence.compactDatafile();
+  }).catch((err) => new Error("remove went wrong" + err));
+};
+
+//This is for deleting a data in database.db
+const neDBRemoveAll = async () => {
+  return new Promise((resolve, reject) => {
+
+    database.remove({}, { multi: true }, (err, numRemoved) => {
+      if (err) reject(err);
+      resolve(numRemoved);
+    });
+    database.persistence.compactDatafile();
+  }).catch((err) => new Error("remove went wrong" + err));
+};
 
 //this is for subscriptions.
 const subscribers = [];
 // whenever newSubscriber gets initialized, they're added to subscribers. See postMessage in graphql
-// to see it being used. 
-const newSubscriber = (fn) => subscribers.push(fn)
+// to see it being used.
+const newSubscriber = (fn) => subscribers.push(fn);
 
-module.exports = {neDBAll, neDBAdd, newSubscriber, neDBRemove, subscribers}
+module.exports = {
+  neDBAll,
+  neDBAdd,
+  newSubscriber,
+  neDBRemove,
+  subscribers,
+  neDBRemoveAll,
+};
--- frontend/component/Room.tsx
+++ frontend/component/Room.tsx
@@ -3,15 +3,24 @@ import { RootStateOrAny, useSelector } from 'react-redux';
 import '../styles/room.styl';
 import Columns from './Columns';
 import {Redirect} from 'react-router-dom'
+import { REMOVE_All } from '../helpers/graphql';
+import { useMutation } from '@apollo/client';
 
 export default function Room() {
+    const [removeAll] = useMutation(REMOVE_All)
     const user = useSelector((state: RootStateOrAny) => state.user.value);
 
     if (!user) {
         return <Redirect to="/" />
     }
 
+    const clearAll = () => {
+        removeAll();
+    }
+
     return <div className="main-container">
         <Columns />
+
+        <span className="clear" onClick={() => clearAll()}>clear</span>
     </div>
 }
